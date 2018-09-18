251641995c4f5f376d39808d3dfbc9bcab5d9ae3
Leo espinoza <leo.spinoza@yahoo.com>
2018-09-18 00:07:20 -0500

    changed app

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// const hbs = require('express-handlebars')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const hbs = require('express-handlebars')
 
-// const session = require('express-session')
-// const FileStore = require('session-file-store')(session)
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 
 
 
